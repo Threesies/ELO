80edd032eb3e83a2711346990d6d7a5f44e730b9
acelentano <celentano_ali@bah.com>
2018-09-28 12:32:36 -0400

    modifications to dropdown

--- ThreesiesDropdown.py
+++ ThreesiesDropdown.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 25 20:37:10 2018
-
 @author: cburns
 """
 #from https://stackoverflow.com/questions/35330224/python-tkinter-xlsxwriter-trying-to-export-user-input-to-excel-file?rq=1
@@ -18,8 +17,10 @@ champs = pd.read_csv('ThreesiesLog.csv')
 
 choices = ['Select Player', 'Carts','AC','MP','Sun Chow','PPJ', 'Fonz', 'Alex', 'D$', 'M1', 'M2', 'Spidey', 'MH', 'Jods']
 
-
 root = tkinter.Tk()
+root.resizable(False, False)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
 e1 = tkinter.StringVar(root)
 e2 = tkinter.StringVar(root)
 e3 = tkinter.StringVar(root)
@@ -40,7 +41,7 @@ export = "CSV"
 
 #Submit Button
 def SubmitEntry():
-    submit = tkinter.messagebox.askquestion("Submit Entry", "Are you sure you want to submit?")
+    submit = tkinter.messagebox.askquestion("Submit Entry", "Submit Game?")
     if submit == "yes":
         player1 = e1.get()
         player2 = e2.get()
@@ -48,7 +49,6 @@ def SubmitEntry():
         currentrow = pd.DataFrame([[player1,player2,winner,strftime("%m-%d-%Y %H:%M", gmtime())]],columns=['Left Side Player','Right Side Player','Winner','Time'])
         global champs
         champs = pd.concat([champs,currentrow],axis=0, ignore_index=True)
-        userconfirm = tkinter.messagebox.showinfo("Save","Your entry has been saved to a " + export + " document!")
         e1.set(choices[0])
         e2.set(choices[0])
         e3.set(choices[0])
@@ -58,34 +58,29 @@ def keypress(event):
         root.destroy()
 
 def QuitEntry():
-    quitask = tkinter.messagebox.askquestion("Quit", "Are you sure you want to quit?")
+    quitask = tkinter.messagebox.askquestion("Quit", "Are you sure the fun is over?")
     if quitask == "yes":
             root.destroy()
             champs.to_csv('ThreesiesLog.csv', index=False)  
+            userconfirm = tkinter.messagebox.showinfo("Save","Your entry has been saved to a " + export + " document!")
 
-firstname = tkinter.Label(root, text="Left Side Player", fg="green")
-secondname = tkinter.Label(root, text="Right Side Player", fg="green")
-winner = tkinter.Label(root, text="WINNER", fg="blue")
+firstname = tkinter.Label(text="Left Side Player", fg="green")
+secondname = tkinter.Label(text="Right Side Player", fg="green")
+winner = tkinter.Label(text="WINNER", fg="blue")
 
-
-TextArea = tkinter.Text()
-ScrollBar = tkinter.Scrollbar(root)
-ScrollBar.config(command=TextArea.yview)
-TextArea.config(yscrollcommand=ScrollBar.set)
-ScrollBar.pack(side='right', fill='y')
-
-Submit = tkinter.Button(root, fg="blue", bg="green", text="Submit", width=50, command=SubmitEntry, activebackground="yellow")
-Quit = tkinter.Button(root, fg="blue", bg="green", text="Quit", width=50, command=QuitEntry,activebackground="yellow")
+Submit = tkinter.Button(root, fg="blue", bg="green", text="Submit Game", width=30, command=SubmitEntry, activebackground="yellow")
+Quit = tkinter.Button(root, fg="blue", bg="green", text="Submit Tournament", width=30, command=QuitEntry,activebackground="yellow")
 
 root.bind_all('<Key>', keypress)
-firstname.pack()
-entry1.pack()
-secondname.pack()
-entry2.pack()
-winner.pack()
-entry3.pack()
-TextArea.pack(expand='YES', fill='both')
-Submit.pack()
-Quit.pack() 
+firstname.grid(row=0, column=0,sticky='e')
+entry1.grid(row=1, column=0, sticky='e')
+secondname.grid(row=0, column=1, sticky='e')
+entry2.grid(row=1, column=1, sticky='e')
+winner.grid(row=2, column=1, sticky='nsew')
+entry3.grid(row=3, column=1, sticky='nsew')
+Submit.grid(row=5, column=1, sticky='nsew')
+Quit.grid(row=6, column=1, sticky='nsew') 
+
+root.grid_rowconfigure(4, minsize=20)
 
 root.mainloop()
